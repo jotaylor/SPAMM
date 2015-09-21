b56a821961c71d8b3eb34cc7df415a4a63780a89
Michael Fausnaugh <fausnaugh@rrlyrae.inside.astronomy.osu.edu>
2015-09-21 11:10:19 -0400

    modified BC template

--- source/spamm/components/BalmerContinuum.py
+++ source/spamm/components/BalmerContinuum.py
@@ -1,13 +1,114 @@
 #!/usr/bin/python
 
-# To create a new component class, copy this file and fill in the values as instructed.
-# Follow comments the begin with "[replace]", then delete the comment.
-
 import sys
 import numpy as np
 from .ComponentBase import Component
+from scipy.interpolate import interp1d
 
-# [replace] "TemplateComponent" with the name of the new component
+import matplotlib.pyplot as plt
+
+#in cgs.  Code genereally assumes that wavelengths are in angstroms,
+#these are taken care of in my helper functions
+c = 2.998e10
+h = 6.626e-27
+k = 1.381e-16
+
+def planckfunc(wv,T):
+	#returns dimensionless
+	#assumes angstroms
+	power = h*c/(k*T*(wv*1.e-8))
+	denom = np.exp(power) - 1
+
+	return 1./(wv)**5/denom
+
+def absorbterm(wv,tau0):
+	#calculates [1 - e^(-tau)] (optically-thin emitting slab)
+	#assumes angstroms
+	tau = tau0*(wv/3646.)**3
+	return 1 - np.exp(-tau)
+
+def balmerseries(n):
+	#central wavelenths of the balmer series
+	#assumes angstroms
+	ilambda = 1./911.3*(0.25 - 1./n**2)
+	return 1./ilambda
+
+def genlines(lgrid,lcent,shift,width):
+	#cacluates the (gaussian) emission line fluxes
+	#lgrid is the array of wavelengths at which to evaluate the line fluxes
+	#lcent is an array with wavelength centers
+	#lshift, lwidth are the offset and the width of the lines
+
+	#to make it go fast, the idea is to give each line it's own
+	#array, so this returns a 2d grid, whith one axis being the
+	#line and the other axis being the fluxes of that line.
+	lcent -= shift*lcent
+	LL = lgrid - lcent.reshape(lcent.size,1)
+	
+	lwidth =  width*lcent.reshape(lcent.size,1)
+	return np.exp(- LL**2 /lwidth**2)
+	
+
+def iratio(l1,l2,T):
+	#assuming LTE, this cacluates the ratio of the line fluxes.
+	#assumes angstroms
+
+	coef = np.genfromtxt('/home/rrlyrae/fausnaugh/repos/mcmc_deconvol/Data/SH95recomb/temp/BLcoeff.dat',usecols=1)
+#	return coef[::-1]
+	dE = h*c*1.e8*(1./l1 - 1./l2)
+	boltzman = np.exp(-dE/k/T)
+
+#	iout = np.r_[coef[::-1],boltzman[48::]*coef[0]]
+	iout = boltzman
+	return iout
+
+
+def makelines(wv,T,shift,width):
+	#Take the helper functions above, and sum the high order balmer lines
+	#H detla is at 6, maybe start higher?
+	N = np.r_[3:51]
+	N = np.r_[3:200]
+	L =  balmerseries(N)
+
+	lines = genlines(wv,L,shift,width)
+	I     = iratio(L,L[47],T)
+
+	scale = np.repeat(I.reshape(I.size,1),lines.shape[1],axis=1)
+	lines *= scale
+
+#	for i in range(lines.shape[0]):
+#		plt.plot(wv,lines[i],'k')
+	lines  = np.sum(lines,axis = 0)
+
+#	plt.plot(wv,lines,'k')
+#	plt.show()
+
+	return lines
+
+def log_conv(x,y,w):
+	lnx  = np.log(x)
+	dlnx = (lnx[-1] - lnx[0])/lnx.size
+	lnxnew = np.r_[lnx[0] : lnx[-1] + dlnx*0.99 : dlnx]
+	lnxnew[-1] = lnx[-1]
+
+	#linear interpolation for now.....
+	interp = interp1d(lnx,y)
+	yrebin = interp(lnxnew)
+
+	if lnx.size %2 == 0:
+		kx = np.r_[-lnx.size//2 : lnx.size//2 + 1]*dlnx
+	else:
+		kx = np.r_[-lnx.size//2 : lnx.size//2 + 2]*dlnx		
+	k  = np.exp(- (kx)**2/(w)**2)
+	k /= np.sum(k)
+
+	ysmooth = np.convolve(yrebin,k,'same')
+
+	interp = interp1d(lnxnew,ysmooth)
+	assert interp(lnx).size == x.size
+
+	return interp(lnx)
+
 class BalmerContinuum(Component):
 	'''
 	Analytic model of the BalmerContinuum (BC) based on Grandi et
@@ -34,14 +135,18 @@ class BalmerContinuum(Component):
 			
 	'''
 	def __init__(self):
-		# [replace] fill in the same name you gave above
 		super(BalmerContinuum, self).__init__()
 
-		# [replace] give the parameters names (spaces are ok), one line for each parameter
-		self.model_parameter_names = list() # this may need to be defined as a method
+		self.model_parameter_names = list()
+
+		# parameters for the continuum
 		self.model_parameter_names.append("normalization")
 		self.model_parameter_names.append("Te")
 		self.model_parameter_names.append("tauBE")
+
+		# paramters for the lines
+		self.model_parameter_names.append("loffset")
+		self.model_parameter_names.append("lwidth")
 		
 		self._norm_wavelength =  None
 		
@@ -54,18 +159,18 @@ class BalmerContinuum(Component):
 
 		self.tauBE_min = None
 		self.tauBE_max = None
+
+		self.loffset_min = None
+		self.loffset_max = None
+
+		self.lwidth_min = None
+		self.lwidth_max = None
 		# etc.
 		
 	@property
 	def is_analytic(self):
 		return True
 	
-
-	def get_norm(self):
-		#use redshift? (are we always fitting in the observed frame?)
-		if self.BCmax is None:
-			self._norm_wavelength = np.median(data_spectrum_wavelength)
-		return self._norm_wavelength
 
 	def initial_values(self, spectrum=None):
 		'''
@@ -83,7 +188,6 @@ class BalmerContinuum(Component):
 		normalization_init = np.random.uniform(low=self.normalization_min,
 						       high=self.normalization_max)
 
-
 		self.Te_min = 5.e3
 		self.Te_max = 20.e3
 		Te_init = np.random.uniform(low=self.Te_min,
@@ -93,16 +197,19 @@ class BalmerContinuum(Component):
 		tauBE_init = np.random.uniform(low=self.tauBE_min,
 					       high=self.tauBE_max)
 
-		return [normalization_init, Te_init, tauBE_init]
+		self.loffset_min = -10.0
+		self.loffset_max =  10.0
+		loffset_init = np.random.uniform( low=self.loffset_min,
+						 high=self.loffset_max)
 
-#	def initialize(self, data_spectrum=None):
-#		'''
-#		Perform any initializations where the data is optional.
-#		'''
-#		if data_spectrum is None:
-#			raise Exception("The data spectrum must be specified to initialize" + 
-#							"{0}.".format(self.__class__.__name__))
-#		self.normalization_wavelength(data_spectrum_wavelength=data_spectrum.wavelengths)
+		self.lwidth_min = 1.0
+		self.lwidth_max = 1000.
+		tauBE_init = np.random.uniform( low=self.lwidth_min,
+					       high=self.lwidth_max)
+
+
+		return [normalization_init, Te_init, tauBE_init,loffset_init,lwidth_init]
+
 
 	def ln_priors(self, params):
 		'''
@@ -136,6 +243,16 @@ class BalmerContinuum(Component):
 		else:
 			ln_priors.append(-1.e17)
 
+		if self.loffset_min < loffset < self.loffset_max:
+			ln_priors.append(np.log(1))
+		else:
+			ln_priors.append(-1.e17)
+
+		if self.lwidth_min < lwidth < self.lwidth_max:
+			ln_priors.append(np.log(1))
+		else:
+			ln_priors.append(-1.e17)
+
 		#reuturn
 		return ln_priors
 		
@@ -148,29 +265,39 @@ class BalmerContinuum(Component):
 		assert len(parameters) == len(self.model_parameter_names), ("The wrong number " +
 									"of indices were provided: {0}".format(parameters))
 		
-		p = self.planckfunc(spectrum.wavelengths,parameters[1])
-		a = self.absorbterm(spectrum.wavelengths,parameters[2])
+		newedge = 3646*(1 - parameters[3]/c)
 
-		pnorm = self.planckfunc(3646.,parameters[1])
-		anorm = self.absorbterm(3646.,parameters[2])
+		p = planckfunc(spectrum.wavelengths,parameters[1])
+		a = absorbterm(spectrum.wavelengths,parameters[2])
+		flux = a*p
+		flux[spectrum.wavelengths > newedge] = 0
+		flux = log_conv(spectrum.wavelengths,flux,parameters[4]/c)
 
-		flux = parameters[0]*p*a/(pnorm*anorm)
+		m = abs(spectrum.wavelengths - newedge) == np.min(abs(spectrum.wavelengths - newedge))
+		fnorm = flux[m]
+
+		flux = parameters[0]*flux/fnorm
+ 
+		lflux = makelines(spectrum.wavelengths,parameters[1],parameters[3]/c,parameters[4]/c)
+		lfnorm = lflux[m]
+
+		norm = parameters[0]/lfnorm
+
+		lflux *= norm
+#		lflux[spectrum.wavelengths <= newedge] = 0
+
+#		plt.plot(spectrum.wavelengths,flux,'b')
+#		plt.plot(spectrum.wavelengths,lflux,'r')
+#		plt.show()
 		
-
-		N = sp.r_[3:250]
-		L =  Balmer(N)
-		lines = self.genlines(spectrum.wavelengths,L)
-		I     = self.Iratio(L,L[4],T_e)
-		lines *= sp.repeat(I.reshape(I.size,1),ploty.shape[1],axis=1)
-		lines  = sp.sum(lines,axis = 0)
-
-		m = abs(spectrum.wavelengths - 3646.) == np.min(abs(spectrum.wavelegnths - 3646.))
-		norm = parameters[0]/lines[m]
-		lines *= norm
-		
-		flux += lines
+		flux += lflux
 
 		return flux
+
+
+
+
+
 
 #	def flux(self, wavelengths=None, parameters=None):
 #		'''
@@ -186,39 +313,3 @@ class BalmerContinuum(Component):
 #		flux = parameters[0]*p*a
 #		
 #		return flux
-
-	@staticmethod
-	def planckfunc(wv,T):
-		#assumes angstroms
-		c = 2.998e10
-		h = 6.626e-27
-		k = 1.381e-16
-
-		power = h*c/(k*T*(wv*1.e-8))
-		denom = np.exp(power) - 1
-
-		return 1./(wv)**5/denom
-
-	@staticmethod
-	def absorbterm(wv,tau0):
-		#assumes angstroms
-		tau = tau0*(wv/3646.)**3
-		return 1 - np.exp(-tau)
-
-	@staticmethod
-	def Balmerseries(n):
-		#assumes angstroms
-		ilambda = 1./912*(0.25 - 1./n**2)
-		return 1./ilambda
-
-	@staticmethod
-	def genlines(lgrid,lcent):
-		#assumes angstroms
-		LL = lgrid - lcent.reshape(lcent.size,1)
-		return sp.exp(- (LL)**2/50.)/sp.sqrt(2*sp.pi*50)
-	
-	@staticmethod
-	def Iratio(l1,l2,T):
-		#assumes angstroms
-		dE = h*c*1.e8*(1./l1 - 1./l2)
-		return sp.exp(- dE/k/T)
