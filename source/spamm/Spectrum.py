2a0dc96dfc2117e2a500c3a153271052cc75e493
Jo Taylor <jotaylor@stsci.edu>
2016-09-30 14:45:06 -0400

    Changed Spectrum class to inherit from Spectrum1DRef

--- source/spamm/Spectrum.py
+++ source/spamm/Spectrum.py
@@ -5,55 +5,53 @@
 
 import scipy
 import numpy as np
+from specutils.core.generic import Spectrum1DRef
+from astropy.units import Quantity
 
-class Spectrum(object):
-	'''
-	The Spectrum object.
-	'''
-	def __init__(self, z=None):
-		'''
-		The Spectrum initialization.
-		
-		@param z Redshift z.
-		'''
-		self._wavelengths = None
-		self._flux = None
-		self.flux_error = None
-		self._norm_wavelength = None
-		self._flux_at_norm_wavelength = None
-	
-	@property
-	def wavelengths(self):
-		return self._wavelengths
-	
-	@wavelengths.setter
-	def wavelengths(self, new_w):
-		self._wavelengths = new_w
-		self._flux_at_norm_wavelength = None
-	
-	@property
-	def flux(self):
-		return self._flux
-	
-	@flux.setter
-	def flux(self, new_flux):
-		self._flux = new_flux
-		self._flux_at_norm_wavelength = None
-	
-	@property
-	def normalization_wavelength(self):
-		if self._norm_wavelength is None:
-			self._norm_wavelength = np.median(self.wavelengths)
-		return self._norm_wavelength
-	
-	def flux_at_normalization_wavelength(self):
-		''' Returns the flux at the normalization wavelength. '''
-		if self._flux_at_norm_wavelength == None:
-			f = scipy.interpolate.interp1d(self.wavelengths, self.flux) # returns function
-			self._flux_at_norm_wavelength = f(self.normalization_wavelength)
-		return self._flux_at_norm_wavelength
-		
-	def grid_spacing(self):
-		''' Return the spacing of the wavelength grid in Ångstroms. Does not support variable grid spacing. '''
-		return self.wavelengths[1] - self.wavelengths[0]
-	
+class Spectrum(Spectrum1DRef):
+    '''
+    Inherit from Spectrum1DRef in specutils.
+    '''
+    def __init__(self, data, wcs=None, *args, **kwargs):
+        '''
+        The Spectrum initialization.
+        
+        @param z Redshift z.
+        '''
+        self.flux_error = None
+        self._norm_wavelength = None
+        self._flux_at_norm_wavelength = None
+        super(Spectrum, self).__init__(data, wcs=wcs, *args, **kwargs)
+        
+    @property
+    def normalization_wavelength(self):
+        if self._norm_wavelength is None:
+            self._norm_wavelength = np.median(self.wavelengths)
+        return self._norm_wavelength
+    
+    def flux_at_normalization_wavelength(self):
+        ''' Returns the flux at the normalization wavelength. '''
+        if self._flux_at_norm_wavelength == None:
+            f = scipy.interpolate.interp1d(self.wavelengths, self.flux) # returns function
+            self._flux_at_norm_wavelength = f(self.normalization_wavelength)
+        return self._flux_at_norm_wavelength
+        
+    def grid_spacing(self):
+        ''' Return the spacing of the wavelength grid in Ångstroms. Does not support variable grid spacing. '''
+        return self.wavelengths[1] - self.wavelengths[0]
+   
+    @property
+    def dispersion(self):
+        """Dispersion quantity with mask applied. Returns a masked array
+        containing a Quantity object."""
+        self._dispersion = super(Spectrum, self).dispersion
+
+        dispersion = np.array(
+            Quantity(self._dispersion, unit=self.dispersion_unit))
+
+        return dispersion
+
+    @dispersion.setter
+    def dispersion(self, new_d):
+        self._dispersion = new_d
+ 
