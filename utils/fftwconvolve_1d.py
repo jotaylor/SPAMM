ac7b84f637435a88f969bacc36eb777119ceb4ad
Jo Taylor <jotaylor@stsci.edu>
2017-10-27 10:40:44 -0400

    Fixed documentation

--- utils/fftwconvolve_1d.py
+++ utils/fftwconvolve_1d.py
@@ -1,16 +1,28 @@
 #! /usr/bin/env python
 
+import pyfftw
+from scipy.fftpack.helper import next_fast_len
+import numpy as np
+
 def fftwconvolve_1d(in1, in2):
     '''
     This code taken from:
-     https://stackoverflow.com/questions/32028979/speed-up-for-loop-in-convolution-for-numpy-3d-array
-     and the resulting output is the full discrete linear convolution of the 
-     inputs(i.e. This returns the convolution at each point of overlap), 
-     which includes additional terms at the start and end of the array such 
-     that if A has size N and B has size M when covolved the size is N+M-1. 
-     At the end-points of the convolution, the signals do not overlap completely, 
-     and boundary effects may be seen.  
+    https://stackoverflow.com/questions/32028979/speed-up-for-loop-in-convolution-for-numpy-3d-array
+    and the resulting output is the full discrete linear convolution of the 
+    inputs (i.e. This returns the convolution at each point of overlap), 
+    which includes additional terms at the start and end of the array such 
+    that if A has size N and B has size M when covolved the size is N+M-1. 
+    At the end-points of the convolution, the signals do not overlap 
+    completely, and boundary effects may be seen. 
+     
+    Args:
+        in1 (array): ?
+        in2 (array): ?
+        
+    Returns:
+        array (array): Linear convolution of inputs.
     '''
+
     outlen = in1.shape[-1] + in2.shape[-1] - 1 
     origlen = in1.shape[-1]
     n = next_fast_len(outlen) 
@@ -22,8 +34,12 @@ def fftwconvolve_1d(in1, in2):
     np.multiply(tr1, tr2, out=pr) 
     out = pyfftw.interfaces.numpy_fft.irfft(pr, n) 
 
+    # Find the central indices of the resulting array
     index_low = int(outlen/2.)-int(np.floor(origlen/2))
     index_high = int(outlen/2.)+int(np.ceil(origlen/2))
-    # these two lines find the central indices of the resulting array
-    return out[..., index_low:index_high].copy() #returns an array the same length as the input 1 and is the appropriate output. Boundary effects are still visible and when overlap is not complete zero values are assumed I believe.
+
+    # Return an array the same length as the input. 
+    # Boundary effects are still visible and when overlap is not 
+    # complete zero values are assumed I believe.
+    return out[..., index_low:index_high].copy() 
 
