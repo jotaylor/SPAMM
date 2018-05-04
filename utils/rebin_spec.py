a9662c5c8db0e51ef920ab30593fc82b4d2f5dbb
Anthea King <anthea.king@unimelb.edu.au>
2018-05-04 12:53:19 +1000

    change of rebin_spec to simple linear interpolation for significant speed up. Only expect subpercent difference in results compared to previous method

--- utils/rebin_spec.py
+++ utils/rebin_spec.py
@@ -1,18 +1,24 @@
 #! /usr/bin/env python
 
-def rebin_spec(wave, specin, wavnew):
-    """
-    Rebin spectra to bins used in wavnew.
-    Ref: http://www.astrobetter.com/blog/2013/08/12/python-tip-re-sampling-spectra-with-pysynphot/
-    """
-    from pysynphot import observation
-    from pysynphot import spectrum as pysynphot_spec
-    import numpy as np
+from numba import jit
+import numpy as np
+#
+@jit(nopython=True)
+def  rebin_spec(old_wave,old_flux,new_wave):
+    I = np.searchsorted(old_wave, new_wave)
+    out_of_range = ((I == 0) | (I == old_wave.size))
+    I[out_of_range] = 1  # Prevent index-errors
 
-    spec = pysynphot_spec.ArraySourceSpectrum(wave=wave, flux=specin)
-    f = np.ones(len(wave))
-    filt = pysynphot_spec.ArraySpectralElement(wave, f, waveunits='angstrom')
-    obs = observation.Observation(spec, filt, binset=wavnew, force='taper')
 
-    return obs.binflux
+    x1 = old_wave[I-1]
+    x2 = old_wave[I]
 
+    y1 = old_flux[I-1]
+    y2 = old_flux[I]
+
+    xI = new_wave
+
+    yI = y1 + (y2-y1) * (xI-x1) / (x2-x1)
+    yI[out_of_range] = 0.
+
+    return yI
