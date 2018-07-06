65977d4292a658e4eca105c021db2369dcbb2dbc
Jo Taylor <jotaylor@stsci.edu>
2018-07-06 14:03:44 -0400

    Used Anthea's find_nearest_index from attempt_to_combine_contComps

--- utils/find_nearest_index.py
+++ utils/find_nearest_index.py
@@ -18,3 +18,17 @@ def find_nearest(input_list, value):
     idx = (np.abs(np.asarray(input_list, dtype = float)-value)).argmin()
     return input_list[idx]
 
+def find_nearest_index(input_list, value):
+    '''
+    Find nearest entry in an array to a specified value.
+    Ref: http://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
+    Args:
+        input_list (list): List of floats.
+        value (float): Desired value to find closest match to in the array.
+    
+    Returns:
+        float (float): Index of value closest to input value from input_list.
+    '''
+    
+    idx = (np.abs(np.asarray(input_list, dtype = float)-value)).argmin()
+    return idx
