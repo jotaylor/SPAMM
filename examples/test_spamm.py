e99e7bcdf17e254ece52676376bcfc6d40bb5faa
Jo Taylor <jotaylor@stsci.edu>
2018-07-25 14:45:06 -0400

    Deleted functions that perform specific component tests, instead use test_spamm which can work with any combo

--- examples/test_spamm.py
+++ examples/test_spamm.py
@@ -3,19 +3,23 @@
 import argparse
 import numpy as np
 
+from utils.parse_pars import parse_pars
 import run_spamm, run_fe, run_nc, run_bc, run_hg
 from utils.add_in_quadrature import add_in_quadrature
 
-# The normalizations are drawn from a gaussian sample with mu=9.06e-15,
-# sigma=3.08946e-15 (from 0->template max flux). fe_width is halfway 
-# between range in parameters. WL is very close to template span (1075-7535)
-WL = np.arange(1000, 10000, 0.5)
+# This should be a wavelength range from 1000-10,000A, every 0.5A
+TEST_WL = parse_pars()["testing"]
+WL = np.arange(TEST_WL["wl_min"], TEST_WL["wl_max"], TEST_WL["wl_step"])
 
+# These values were picked by hand to provide the most realistic power law.
 NC_PARAMS = {"wl": WL,
              "slope1": 2.3,
              "norm_PL": 5e-15,
              "broken_pl": False}
 
+# The normalizations are drawn from a gaussian sample with mu=9.06e-15,
+# sigma=3.08946e-15 (from 0->template max flux). fe_width is halfway 
+# between range in parameters. WL is very close to template span (1075-7535)
 FE_PARAMS = {"fe_norm_1": 1.07988504e-14,
              "fe_norm_2": 6.91877436e-15,
              "fe_norm_3": 5e-15,# 8.68930476e-15, 
@@ -42,116 +46,6 @@ HG_PARAMS = {"hg_norm_1": 5.e-17,
              "wl": WL}
 
 LINEOUT = "#"*75
-
-#-----------------------------------------------------------------------------#
-
-def test_nc_fe(fe_params=FE_PARAMS):
-    print("{0}\nTESTING NUCLEAR CONTINUUM + IRON\n{0}".format(LINEOUT))
-    #fe_wl, fe_flux, fe_err, fe_p = run_fe.from_file("/user/jotaylor/git/spamm/Data/FakeData/Iron_comp/fakeFe1_deg.dat", redshift=0.5)
-    fe_wl, fe_flux, fe_err, fe_p = run_fe.create_fe(fe_params)
-    nc_wl, nc_flux, nc_err, nc_p = run_nc.create_nc(NC_PARAMS)
-    assert set(nc_wl-fe_wl) == {0}, "Wavelength scales do not match" 
-    comb_wl = WL
-    comb_flux = nc_flux + fe_flux
-    comb_err = add_in_quadrature([nc_err, fe_err])
-
-    comb_p = {**nc_p, **fe_p}
-
-    run_spamm.spamm_wlflux({"PL": True, "FE": True}, comb_wl, comb_flux, comb_err,
-                           comp_params=comb_p, n_walkers=100, n_iterations=500)
-                           #, pname="nc_fe.pickle.gz")
-
-
-#-----------------------------------------------------------------------------#
-
-def test_fe_fromfile(datafile="/user/jotaylor/git/spamm/Data/FakeData/Iron_comp/fakeFe1_deg.dat", 
-                     redshift=0.5):
-    print("{0}\nTESTING IRON\n{0}".format(LINEOUT))
-    fe_wl, fe_flux, fe_err, fe_p = run_fe.from_file(datafile, redshift)
-    run_spamm.spamm_wlflux({"FE": True}, fe_wl, fe_flux, fe_err,
-                           comp_params=fe_p)#, pname="fe.pickle.gz")
-
-#-----------------------------------------------------------------------------#
-
-def test_fe(fe_params=None):
-    print("{0}\nTESTING IRON\n{0}".format(LINEOUT))
-    fe_wl, fe_flux, fe_err, fe_p = run_fe.create_fe(fe_params)
-    
-    run_spamm.spamm_wlflux({"FE": True}, fe_wl, fe_flux, fe_err, 
-                           comp_params=fe_p)
-
-#-----------------------------------------------------------------------------#
-
-def test_nc():
-    print("{0}\nTESTING NUCLEAR CONTINUUM\n{0}".format(LINEOUT))
-    nc_wl, nc_flux, nc_err, nc_p = run_nc.combine_pl()
-    run_spamm.spamm_wlflux({"PL": True}, nc_wl, nc_flux, nc_err,
-                           comp_params=nc_p)
-                           #pname="nc.pickle.gz")
-
-#-----------------------------------------------------------------------------#
-
-def test_bc_fromfile(datafile="/user/jotaylor/git/spamm/Data/FakeData/BaC_comp/FakeBac_lines04_deg.dat", 
-                     redshift=0.2, bpc=True):
-    print("{0}\nTESTING BALMER CONTINUUM\n{0}".format(LINEOUT))
-    bc_wl, bc_flux, bc_err, bc_p = run_bc.from_file(datafile, redshift)
-    run_spamm.spamm_wlflux({"BC": True, "BpC": True}, bc_wl, bc_flux, bc_err, 
-                           comp_params=bc_p, pname="bc.pickle.gz")
-
-#-----------------------------------------------------------------------------#
-
-def test_bc(bc_params=None):
-    print("{0}\nTESTING BALMER CONTINUUM\n{0}".format(LINEOUT))
-    bc_wl, bc_flux, bc_err, bc_p = run_bc.create_bc(bc_params)
-
-    run_spamm.spamm_wlflux({"BC": True, "BpC": True}, bc_wl, bc_flux, bc_err, 
-                           n_walkers=100, n_iterations=500, comp_params=bc_p)
-
-#-----------------------------------------------------------------------------#
-
-def test_nc_bc(bc_params=BC_PARAMS):
-    print("{0}\nTESTING NUCLEAR + BALMER CONTINUUM\n{0}".format(LINEOUT))
-    bc_wl, bc_flux, bc_err, bc_p = run_bc.create_bc(bc_params)
-    nc_wl, nc_flux, nc_err, nc_p = run_nc.create_nc(NC_PARAMS)
-    assert set(nc_wl-bc_wl) == {0}, "Wavelength scales do not match" 
-    comb_wl = WL
-    comb_flux = nc_flux + bc_flux
-    comb_err = add_in_quadrature([nc_err, bc_err])
-
-    comb_p = {**nc_p, **bc_p}
-    
-    print(comb_p) 
-    run_spamm.spamm_wlflux({"PL": True, "BC": True, "BpC": True}, comb_wl, comb_flux, comb_err,
-                           comp_params=comb_p)#, n_walkers=100, n_iterations=500)
-
-
-#-----------------------------------------------------------------------------#
-
-def test_nc_bc_fe(bc_params=BC_PARAMS, fe_params=FE_PARAMS):
-    print("{0}\nTESTING NUCLEAR + BALMER CONTINUUM + IRON\n{0}".format(LINEOUT))
-    fe_wl, fe_flux, fe_err, fe_p = run_fe.create_fe(fe_params)
-    bc_wl, bc_flux, bc_err, bc_p = run_bc.create_bc(bc_params)
-    nc_wl, nc_flux, nc_err, nc_p = run_nc.create_nc(NC_PARAMS)
-    assert set(nc_wl-bc_wl) == {0}, "NC and BC wavelength scales do not match" 
-    assert set(nc_wl-fe_wl) == {0}, "NC and Fe wavelength scales do not match" 
-    comb_wl = WL
-    comb_flux = nc_flux + bc_flux + fe_flux
-    comb_err = add_in_quadrature([nc_err, bc_err, fe_err])
-
-    comb_p = {**nc_p, **bc_p, **fe_p}
-
-    print(comb_p) 
-    run_spamm.spamm_wlflux({"PL": True, "BC": True, "BpC": True, "FE": True}, comb_wl, comb_flux, comb_err,
-                           comp_params=comb_p, n_walkers=400, n_iterations=400)
-
-#-----------------------------------------------------------------------------#
-
-def test_hg(hg_params=HG_PARAMS):
-    print("{0}\nTESTING HOST GALAXY\n{0}".format(LINEOUT))
-    hg_wl, hg_flux, hg_err, hg_p = run_hg.create_hg(hg_params)
-    
-    run_spamm.spamm_wlflux({"HOST": True}, hg_wl, hg_flux, hg_err, 
-                           comp_params=hg_p) 
 
 #-----------------------------------------------------------------------------#
 
@@ -228,14 +122,6 @@ def parse_comps(argcomp):
 
 
 if __name__ == "__main__":
-#    test_nc_fe()
-#    test_nc()
-#    test_fe()
-#    test_bc()
-#    test_nc_bc()
-#    test_nc_bc_fe()
-#    test_hg()
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--comp", nargs="*", 
                         help="List of components to use: can be  PL, FE, BC, HG")
