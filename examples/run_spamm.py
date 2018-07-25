c5004acd3f239cac19e032f03c88be75114eb38c
Jo Taylor <jotaylor@stsci.edu>
2018-07-25 15:41:53 -0400

    Cleaned up code, removed unused packages, added documentation

--- examples/run_spamm.py
+++ examples/run_spamm.py
@@ -3,13 +3,7 @@
 import gzip
 import dill as pickle
 import datetime
-import argparse
-import numpy as np
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as pl
 
-from utils.parse_pars import parse_pars
 from analyze_model_run import make_chain_plots
 from spamm.Spectrum import Spectrum
 from spamm.Model import Model
@@ -18,27 +12,37 @@ from spamm.components.HostGalaxyComponent import HostGalaxyComponent
 from spamm.components.FeComponent import FeComponent
 from spamm.components.BalmerContinuumCombined import BalmerCombined
 from spamm.components.ReddeningLaw import Extinction
-from spamm.components.MaskingComponent import Mask
 
 #-----------------------------------------------------------------------------#
 
 def spamm_wlflux(components, wl, flux, flux_err=None, 
-                 n_walkers=30, n_iterations=500, brokenPL=False,
+                 n_walkers=30, n_iterations=500, 
                  pname=None, comp_params=None):
     """
     Args:
-        components : dictionary
-            A dictionary with at least one component to model, e.g. {"FE": True}
-        datafile : str
-            Pathname of the datafile to be used as input.
+        components (dictionary): A dictionary with at least one component to 
+            model, e.g. {"FE": True}. Accepted key values are:
+                - PL
+                - FE
+                - HOST
+                - BC
+                - BpC
+                - Calzetti_ext
+                - SMC_ext
+                - MW_ext
+                - AGN_ext
+                - LMC_ext
+        wl (array-like): Wavelength of input data spectrum.
+        flux (array-like): Flux of input data spectrum.
+        flux_err (array-like): Error on flux measurement.
+        n_walkers (int): Number of walkers, or chains, to use in emcee.
+        n_iterations (int): Number of iterations for each walker/chain.
+        pname (str): Name of output pickle file. If None, name will be
+            determined based on current run time.
         comp_params : dictionary
-            If None, the component parameters (at minimum- wavelength, flux, flux error)
-            will be determined from the datafile. If defined, comp_params *MUST* contain:
-                - wl
-                - flux
-                - err
-                - pname (can be None)
-                - broken_pl (if component=PL)
+            Contains the known values of component parameters, with keys
+            defined in each of the individual run scripts (run_XX.py).
+            If None, the actual values of parameters will not be plotted.
     """
 
     t1 = datetime.datetime.now()
@@ -53,6 +57,12 @@ def spamm_wlflux(components, wl, flux, flux_err=None,
     spectrum.dispersion = wl#*units.angstrom
     spectrum.flux_error = flux_err    
     
+    if comp_params is None:
+        comp_params = {}
+    for k,v in zip(("wl", "flux", "err", "components"), (wl, flux, flux_err, components)):
+        if k not in comp_params:
+            comp_params[k] = v
+    
     # ------------
     # Initialize model
     # ------------
@@ -63,11 +73,16 @@ def spamm_wlflux(components, wl, flux, flux_err=None,
     # Initialize components
     # -----------------
     if components["PL"]:
-        if brokenPL:
-            nuclear_comp = NuclearContinuumComponent(broken=True)
-        else:
-            nuclear_comp = NuclearContinuumComponent()
-        model.components.append(nuclear_comp)
+        try:
+            if comp_params["broken_pl"] is True:
+                brokenPL = True
+            else:
+                brokenPL = False
+        except:
+            brokenPL = False
+        finally:
+            nuclear_comp = NuclearContinuumComponent(broken=brokenPL)
+            model.components.append(nuclear_comp)
     if components["FE"]:
         fe_comp = FeComponent()
         model.components.append(fe_comp)
@@ -81,12 +96,6 @@ def spamm_wlflux(components, wl, flux, flux_err=None,
     if components["Calzetti_ext"] or components["SMC_ext"] or components["MW_ext"] or components["AGN_ext"] or components["LMC_ext"]:
         ext_comp = Extinction(MW=MW_ext, AGN=AGN_ext, LMC=LMC_ext, SMC=SMC_ext, Calzetti=Calzetti_ext)
         model.components.append(ext_comp)
-
-    if comp_params is None:
-        comp_params = {}
-    for k,v in zip(("wl", "flux", "err", "components"), (wl, flux, flux_err, components)):
-        if k not in comp_params:
-            comp_params[k] = v
 
     model.data_spectrum = spectrum # add data
     
