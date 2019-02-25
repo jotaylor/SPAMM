f79c63c00ca9ac792989e2665f285c211381a4b4
Jo Taylor <jotaylor@stsci.edu>
2019-02-25 15:42:21 -0500

    Rewrite Spectrum to use new specutils packages and update child class variables accordingly. Make spectral_axis and flux settable. Remove redundant wavelenghts and dispersion attributes

--- spamm/Spectrum.py
+++ spamm/Spectrum.py
@@ -5,81 +5,74 @@
 
 import scipy
 import numpy as np
-from specutils.core.generic import Spectrum1DRef
+from specutils import Spectrum1D
 from astropy.units import Quantity
 
-class Spectrum(Spectrum1DRef):
+from utils.parse_pars import parse_pars
+FLUX_UNIT = parse_pars()["global"]["flux_unit"]
+WL_UNIT = parse_pars()["global"]["wl_unit"]
+
+#-----------------------------------------------------------------------------#
+
+class Spectrum(Spectrum1D):
     '''
     Inherit from Spectrum1DRef in specutils.
     '''
-    def __init__(self, data, wcs=None, *args, **kwargs):
-        '''
-        The Spectrum initialization.
-        @param z Redshift z.
-        '''
-        #self.flux_error = None
+    def __init__(self, spectral_axis=None, flux=None, spectral_axis_unit=WL_UNIT, 
+                 flux_unit=FLUX_UNIT, *args, **kwargs):
+        self.flux_unit = flux_unit
+        # Not sure these are needed
         self._norm_wavelength = None
         self._norm_wavelength_flux = None
-        super(Spectrum, self).__init__(data, wcs=wcs, *args, **kwargs)
-        self.wavelengths = None
+        # Set equal to None in order to inherit from Spectrum while overriding
+        # ability to set/get in child class here.
+        self._spectral_axis = None
+        self._flux = None
+        super(Spectrum, self).__init__(spectral_axis=spectral_axis*spectral_axis_unit, 
+                                       flux=flux*flux_unit, *args, **kwargs)
 
     @property
     def norm_wavelength(self):
         if self._norm_wavelength is None:
-            self._norm_wavelength = np.median(self.wavelengths)
+            self._norm_wavelength = np.median(self.spectral_axis)
         return self._norm_wavelength
 
     @property
     def norm_wavelength_flux(self):
         ''' Returns the flux at the normalization wavelength. '''
         if self._norm_wavelength_flux == None:
-            f = scipy.interpolate.interp1d(self.wavelengths, self.flux) # returns function
-            self._norm_wavelength_flux = f(self.norm_wavelength)
+            f = scipy.interpolate.interp1d(self.spectral_axis, self.flux) # returns function
+            self._norm_wavelength_flux = Quantity(f(self.norm_wavelength), self.flux_unit)
         return self._norm_wavelength_flux
 
     def grid_spacing(self):
         ''' Return the spacing of the wavelength grid in Ångstroms. Does not support variable grid spacing. '''
-        return self.wavelengths[1] - self.wavelengths[0]
+        return self.spectral_axis[1] - self.spectral_axis[0]
 
     @property
-    def dispersion(self):
-        self._dispersion = super(Spectrum, self).dispersion
+    def spectral_axis(self):
+        # This is necessary to override parent class inability to set/get
+        if self._spectral_axis == None:
+            self._spectral_axis = super(Spectrum, self).spectral_axis
+        return self._spectral_axis
 
-        dispersion = np.array(
-            Quantity(self._dispersion, unit=self.dispersion_unit))
-
-        return dispersion
-
-    @dispersion.setter
-    def dispersion(self, new_d):
-        self._dispersion = new_d
-        self._wavelengths = new_d
-
-    @property
-    def wavelengths(self):
-        wavelengths = self._dispersion
-        
-        return wavelengths
-
-    @wavelengths.setter
-    def wavelengths(self, new_wl):
-        self._wavelengths = new_wl
-        self._dispersion = new_wl
+    @spectral_axis.setter
+    def spectral_axis(self, new_wl):
+        spectral_axis = Quantity(new_wl, unit=self.spectral_axis_unit)
+        self._spectral_axis = spectral_axis
+        self._norm_wavelength = None
 
     @property
     def flux(self):
-        self._flux = super(Spectrum, self).flux
-        flux = np.array(Quantity(self._flux, unit=self.unit))
-        flux = np.ma.masked_array(flux,mask=self.mask)
-        return flux
+        # This is necessary to override parent class inability to set/get
+        if self._flux == None:
+            self._flux = super(Spectrum, self).flux
+        return self._flux
 
     @flux.setter
     def flux(self, new_flux):
-#        self._flux = new_flux
-        self._data = new_flux
+        flux = Quantity(new_flux, unit=self.flux_unit)
+        self._flux = flux
         self._norm_wavelength_flux = None
 
-#    def bin_spectrum(self):
-#TODO need to finalize this 
-
-#TODO need to add log_grid_spacing
+#TODO need to add log_grid_spacing, spectrum binning
