4e9659cc6a92ab8d0ea227e3f041d5136d5c9bd0
Jo Taylor <jotaylor@stsci.edu>
2019-05-22 16:19:58 -0400

    Added option for output directory and dev command line interface

--- spamm/spamm.py
+++ spamm/spamm.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import gzip
 import dill as pickle
 import datetime
@@ -22,7 +23,7 @@ ACCEPTED_COMPS = ["PL", "FE", "HOST", "BC", "BPC", "CALZETTI_EXT", "SMC_EXT", "M
 #-----------------------------------------------------------------------------#
 
 def spamm(complist, inspectrum, par_file=None, n_walkers=30, 
-          n_iterations=500, pname=None, comp_params=None):
+          n_iterations=500, outdir=None, picklefile=None, comp_params=None):
     """
     Args:
         complist (list): A list with at least one component to model. 
@@ -44,7 +45,9 @@ def spamm(complist, inspectrum, par_file=None, n_walkers=30,
         par_file (str): Location of parameters file.
         n_walkers (int): Number of walkers, or chains, to use in emcee.
         n_iterations (int): Number of iterations for each walker/chain.
-        pname (str): Name of output pickle file. If None, name will be
+        outdir (str): Name of output directory for pickle file and plots.
+            If None, name will be determined based on current run tie.
+        picklefile (str): Name of output pickle file. If None, name will be
             determined based on current run time.
         comp_params : dictionary
             Contains the known values of component parameters, with keys
@@ -134,15 +137,56 @@ def spamm(complist, inspectrum, par_file=None, n_walkers=30,
     p_data = {"model": model,
               "comp_params": comp_params}
 
-    if pname is None:
-        now = datetime.datetime.now()
-        pname = "model_{0}.pickle.gz".format(now.strftime("%Y%m%d_%M%S"))
+    if picklefile is None:
+        nowdt = datetime.datetime.now()
+        now = nowdt.strftime("%Y%m%d_%M%S")
+        picklefile = "model_{0}.pickle.gz".format(now)
+    if not picklefile.endswith(".gz"):
+        picklefile += ".gz"
+    if outdir is None:
+        outdir = now
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    pname = os.path.join(outdir, picklefile)
+    
     with gzip.open(pname, "wb") as model_output:
         model_output.write(pickle.dumps(p_data))
         print("Saved pickle file {0}".format(pname))
-    make_plots_from_pickle(pname)
+    make_plots_from_pickle(pname, outdir)
 
     t2 = datetime.datetime.now()
     print("executed in {}".format(t2-t1))
 
     return p_data
+
+#-----------------------------------------------------------------------------#
+
+def parse_comps(argcomp):
+    if len(argcomp) == 1:
+        if "," in argcomp[0]:
+            comps = [x for x in argcomp[0].split(",")]
+        else:
+            comps = argcomp
+    else:
+        comps = argcomp
+
+    return comps
+
+#-----------------------------------------------------------------------------#
+# NOT SUPPORTED YET #
+
+#if __name__ == "__main__":
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument("inspectrum", help="Input spectrum file", 
+#                        type=str) 
+#    parser.add_argument("--comp", nargs="*",
+#                        help="List of components to use: can be  PL, FE, BC, HG")
+#    parser.add_argument("--n_walkers", dest="n_walkers", default=30,
+#                        help="Number of walkers")
+#    parser.add_argument("--n_iterations", dest="n_iterations", default=500,
+#                        help="Number of iterations per walker")
+#    args = parser.parse_args()
+#
+#
+#    comps = parse_comps(args.comp)
+#    spamm(complist=comps, n_walkers=int(args.n_walkers), n_iterations=int(args.n_iterations))
