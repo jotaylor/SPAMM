498a35865855ffba998cbbe026232de7a080a3a3
Jo Taylor <jotaylor@stsci.edu>
2019-05-23 11:42:49 -0400

    Fix spamm import

--- examples/test_spamm.py
+++ examples/test_spamm.py
@@ -4,7 +4,7 @@ import argparse
 import numpy as np
 
 from utils.parse_pars import parse_pars
-from spamm import spamm
+from spamm.run_spamm import spamm
 import run_fe, run_nc, run_bc, run_hg
 from utils.add_in_quadrature import add_in_quadrature
 
@@ -102,9 +102,9 @@ def test_spamm(components=None, comp_params=None, n_walkers=30, n_iterations=500
 
 #    return comb_wl, comb_flux, comb_err, all_fluxes
 
-    p_data = spamm.spamm(comp_names, (comb_wl, comb_flux, comb_err), 
-                                    comp_params=comb_p, n_walkers=n_walkers,
-                                    n_iterations=n_iterations)
+    p_data = spamm(comp_names, (comb_wl, comb_flux, comb_err), 
+                      comp_params=comb_p, n_walkers=n_walkers,
+                      n_iterations=n_iterations)
 
     return p_data
 
